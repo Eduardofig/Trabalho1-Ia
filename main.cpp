e9ecf16a2759e0ab49754a77419d19d5987ccfbd
Eduardofig <duzinho039@gmail.com>
2022-10-15 13:12:35 -0300

    :white_check_mark: Experimento bom

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    knn_graph graph(30, 1);
+    knn_graph graph(30, 2);
     aux::print_graph(graph);
-    aux::run_experiment(graph, 3, 30);
+    aux::run_experiment(graph, 1, 30);
 }
