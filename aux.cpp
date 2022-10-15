66fa44713091e15fe901098e5dd021ebbcc91bb8
Eduardofig <duzinho039@gmail.com>
2022-10-15 12:11:36 -0300

    :sparkles: Print of distance of path

--- aux.cpp
+++ aux.cpp
@@ -43,7 +43,9 @@ void aux::run_experiment(knn_graph &graph, int st, int target)
 
     std::cout << "\nDfs:\n";
     const auto &[dfs_path, dfs_walked] = search::dfs(graph, st, target);
+    const double dfs_dist = aux::get_path_distance(graph, dfs_path);
     aux::print_path(dfs_path);
+    std::cout << "Distance: " << dfs_dist << '\n';
     std::cout << "Walked: " << dfs_walked << '\n';
 
     for(int i = 0; i < 100; ++i) {
@@ -52,8 +54,11 @@ void aux::run_experiment(knn_graph &graph, int st, int target)
 
     std::cout << "\nDijkstra:\n";
     const auto &[dijk_path, dijk_walked] = search::dijkstra(graph, st, target);
+    const double dijk_dist = aux::get_path_distance(graph, dijk_path);
     aux::print_path(dijk_path);
+    std::cout << "Distance: " << dijk_dist << '\n';
     std::cout << "Walked: " << dijk_walked << '\n';
+
 
     for(int i = 0; i < 100; ++i) {
         std::cout << '-';
@@ -61,7 +66,9 @@ void aux::run_experiment(knn_graph &graph, int st, int target)
 
     std::cout << "\nBest First:\n";
     const auto &[best_first_path, best_first_walked] = search::best_first(graph, st, target);
+    const double best_first_dist = aux::get_path_distance(graph, best_first_path);
     aux::print_path(best_first_path);
+    std::cout << "Distance: " << best_first_dist << '\n';
     std::cout << "Walked: " << best_first_walked << '\n';
 
     for(int i = 0; i < 100; ++i) {
@@ -70,7 +77,9 @@ void aux::run_experiment(knn_graph &graph, int st, int target)
 
     std::cout << "\nA*:\n";
     const auto &[astar_path, astar_walked] = search::astar(graph, st, target);
+    const double astar_dist = aux::get_path_distance(graph, astar_path);
     aux::print_path(astar_path);
+    std::cout << "Distance: " << astar_dist << '\n';
     std::cout << "Walked: " << astar_walked << '\n';
 
     for(int i = 0; i < 100; ++i) {
@@ -96,4 +105,19 @@ void aux::print_graph(knn_graph &graph)
     for(int i = 0; i < 100; ++i) {
         std::cout << '-';
     }
+}
+
+double aux::get_path_distance(knn_graph &graph, const std::vector<int> &path)
+{
+    double dist = 0;
+
+    int n = path.size();
+    for(int i = 0; i < n - 1; ++i) {
+        int from = path[i];
+        int to = path[i + 1];
+
+        dist += aux::dist(from, to, graph.vertices);
+    }
+
+    return dist;
 }
--- aux.hpp
+++ aux.hpp
@@ -13,4 +13,5 @@ namespace aux
     void print_path(const std::vector<int> &path);
     void run_experiment(knn_graph &graph, int st, int target);
     void print_graph(knn_graph &graph);
+    double get_path_distance(knn_graph &graph, const std::vector<int> &path);
 }
