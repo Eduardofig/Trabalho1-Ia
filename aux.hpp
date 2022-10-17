7058ecd7a79fee4aeef7863e318d7f7874ac8907
danielcarvalho99 <danielcd3099@gmail.com>
2022-10-17 13:30:44 -0300

    aux.hpp documented

--- aux.hpp
+++ aux.hpp
@@ -11,10 +11,19 @@
 
 namespace aux
 {
+    // Distância de um nó a outro
     double dist(const int from, const int to, const std::vector<std::pair<int, int>> &vertices);
+    
+    // Exibe o caminho encontrado
     void print_path(const std::vector<int> &path);
+    
+    // Roda o experimento para os diferentes algoritmos
     void run_experiment(knn_graph &graph, int st, int target);
+    
+    // Exibe nosso grafo KNN
     void print_graph(knn_graph &graph);
+    
+    // Retorna a distância percorrida do caminho
     double get_path_distance(knn_graph &graph, const std::vector<int> &path);
     std::pair<double, double> get_avg_time_dist(knn_graph &graph, int num_experiments,
             std::pair<std::vector<int>, double> (*search_function)(knn_graph&, int, int));
